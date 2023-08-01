bed39d15affe71b939bad5bc11c1f80c0b2177c6
EliadeSantos <eliade.barbosa@gmail.com>
2023-08-01 10:05:37 -0300

    voltando as alterações antes do novo console log

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
-// mais um console log de teste:
-console.log("Feito para testar o pull no diretorio local");
